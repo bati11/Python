895a6883e9b13dca1d81315a29145c11b0033b36
Bati11 <ba0548574418@gmail.com>
2025-06-23 17:27:48 +0300

    remove lesson2 for test

--- lesson2/book.py
+++
@@ -1,8 +0,0 @@
-class Book:
-    def __init__(self, title, author):
-        if not title or not author:
-            raise ValueError("Title and author must not be empty.")
-        self.title = title
-        self.author = author
-        self.is_checked_out = False
-
